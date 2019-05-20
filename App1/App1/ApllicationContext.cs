fe7a3f098778f2360ca140f36ba9f1c21133d100
SkyMan <mr.skyman65@gmail.com>
2019-05-21 00:37:28 +0300

    More fix

--- App1/App1/ApllicationContext.cs
+++ App1/App1/ApllicationContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- App1/App1/DbModel.cs
+++ App1/App1/DbModel.cs
@@ -7,6 +7,7 @@ namespace DragonRun
 {
    public class DbModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Score { get; set; }
     }
